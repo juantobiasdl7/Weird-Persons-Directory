cdbb846c79607c1339ebbe9360defbffc06b66c3
Juan Tobias <juantobiasdl7@gmail.com>
2020-04-04 14:14:57 -0600

    Updates

---
+++ precache-manifest.5aa008562c68f35615665e7b77c27a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb20401f7d8caa8fddcfae7e1dad8dce",
+    "url": "/Weird-Persons-Directory/index.html"
+  },
+  {
+    "revision": "8ce4d28d675e2cd02053",
+    "url": "/Weird-Persons-Directory/static/css/main.b2a5ac9f.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/Weird-Persons-Directory/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weird-Persons-Directory/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce4d28d675e2cd02053",
+    "url": "/Weird-Persons-Directory/static/js/main.e6db2ec3.chunk.js"
+  },
+  {
+    "revision": "8ff93ac73bd917453804",
+    "url": "/Weird-Persons-Directory/static/js/runtime-main.414a4c96.js"
+  }
+]);
